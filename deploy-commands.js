7774df1b0579b2992d15b72c65bf34515420e3af
Jaxz10 <42484410+Jaxz102@users.noreply.github.com>
2022-03-12 04:15:04 -0500

    messages

---
+++ deploy-commands.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const token = process.env.TOKEN;
+const clientId = process.env.CLIENTID;
+const guildId = process.env.GUILDID;
+
+const commands = [
+	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
+	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
+	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
+].map(command => command.toJSON());
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
--- index.js
+++ index.js
@@ -2,15 +2,48 @@ require('dotenv').config();
 // Require the necessary discord.js classes
 const { Client, Intents } = require('discord.js');
 const token = process.env.TOKEN;
+const clientId = process.env.CLIENTID;
+const guildId = process.env.GUILDID;
 
-// Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+// const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES"] });
+const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES"], partials: ["CHANNEL"] });
 
-// When the client is ready, run this code (only once)
+
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
+
+client.on('interactionCreate', async interaction => {
+    console.log("WORKEDD")
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+    
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply('Server info.');
+	} else if (commandName === 'user') {
+		await interaction.reply('User info.');
+	}
+});
+
+client.on("messageCreate", async message => {
+
+   
+    const content = message.content
+    console.log(content)
+
+    if (content === 'ping') {
+		await message.reply('Pong!');
+	} else if (content === 'server') {
+		await message.reply('Server info.');
+	} else if (content === 'user') {
+		await message.reply('User info.');
+	}
+});
 // Login to Discord with your client's token
-console.log(token);
+
 client.login(token);
+
