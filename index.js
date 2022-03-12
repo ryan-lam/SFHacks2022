b2d0a63e1ffc7cadb6e8ad8839a6c05389973272
ryan-lam <ryan-lam@users.noreply.github.com>
2022-03-12 18:26:49 -0500

    tf now working to detect toxic and not toxic msgs

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 // Require the necessary discord.js classes
 const fs = require('node:fs');
-const { Client, Collection, Intents } = require('discord.js');
+const { Client, Collection, Intents, MessageActionRow, MessageButton, MessageEmbed } = require('discord.js');
 const wait = require('node:timers/promises').setTimeout;
 const token = process.env.TOKEN;
 const clientId = process.env.CLIENTID;
@@ -58,20 +58,42 @@ client.on('interactionCreate', async interaction => {
 // For normal messaging
 client.on("messageCreate", async message => {
     const content = message.content
-    console.log(content)
+	console.log(`New message from ${message.author.username}: "${content}"`)
 
 	// if (!message.author.bot) {
-	if (message.author.id == 272477288640151552) {
+	// if (message.author.id == 272477288640151552) {
+	if (message.author.id == 272477288640151552 || message.author.id == 290951901187538947) {
 		console.log("Checking for toxicity")
 		const toxicityThreshold = 0.8
 		await toxicity.load(toxicityThreshold).then(model => {
 			model.classify([content]).then(data => {
-				const identity_attack = data[0].results[0].match
-				const insult = data[1].results[0].match
-				const obscene = data[2].results[0].match
-				const severe_toxicity = data[3].results[0].match
-				const sexual_explicit = data[4].results[0].match
-				message.reply(`identity_attack:${identity_attack}\ninsult:${insult}\nobscene:${obscene}\nsevere_toxicity:${severe_toxicity}\nsexual_explicit:${sexual_explicit}`)
+				const identity_attack = ["Identity Attack", data[0].results[0].match]
+				const insult = ["Insult", data[1].results[0].match]
+				const obscene = ["Obscene", data[2].results[0].match]
+				const severe_toxicity = ["Severe Toxicity", data[3].results[0].match]
+				const sexual_explicit = ["Sexual Explicit", data[4].results[0].match]
+				const listOfToxicity = [identity_attack, insult, obscene, severe_toxicity, sexual_explicit]
+				.filter((arr) => arr[1]).map((arr) => arr[0])
+				console.log(listOfToxicity)
+
+				if (listOfToxicity.length) {
+					listOfToxicity.join(', ')
+					const embed = new MessageEmbed()
+						.setColor('#FF0000')
+						.setTitle('TOXICITY WARNING!')
+						.setDescription(`Your has been identified as the following toxic behaviour(s): \n${listOfToxicity}`)
+					const row = new MessageActionRow().addComponents(
+						new MessageButton()
+							.setCustomId('DANGER')
+							.setLabel('DANGER')
+							.setStyle('DANGER'),
+					);
+					message.reply({ ephemeral: true, embeds: [embed], components: [row] });
+
+				} else {
+					console.log("No toxicity detected")
+				}
+
 			})
 		})
 	}
